2dc1e7eb5250b4d9719592b963fef6bbcdd562a2
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-09 16:58:57 +0100

    fix: setup

--- functions/discord-bot/src/setup.js
+++ functions/discord-bot/src/setup.js
@@ -1,26 +1,36 @@
 import { Discord } from './discord.js';
 
+const CommandType = {
+  SLASH_COMMAND: 1,
+};
+
+const CommandOptionType = {
+  STRING: 3,
+};
+
 async function setup() {
   const discord = new Discord();
 
   try {
     await discord.registerCommand({
       name: 'hello',
+      type: CommandType.SLASH_COMMAND,
       description: 'Hello command',
     });
 
     await discord.registerCommand({
       name: 'schedule',
+      type: CommandType.SLASH_COMMAND,
       description: 'Schedule message command',
       options: [
         {
-          type: 3,
+          type: CommandOptionType.STRING,
           name: 'message',
           description: 'The message to send',
           required: true,
         },
         {
-          type: 3,
+          type: CommandOptionType.STRING,
           name: 'delay',
           description: 'The delay to send the message - e.g. 1h30m',
           required: true,
@@ -30,6 +40,7 @@ async function setup() {
 
     await discord.registerCommand({
       name: 'start',
+      type: CommandType.SLASH_COMMAND,
       description: 'Start your day command',
       options: {
         type: 3,
@@ -40,9 +51,10 @@ async function setup() {
 
     await discord.registerCommand({
       name: 'daily',
+      type: CommandType.SLASH_COMMAND,
       description: 'Auto format your daily update',
       options: {
-        type: 3,
+        type: CommandOptionType.STRING,
         name: 'message',
         description: 'The daily update message',
       },
