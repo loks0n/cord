a005d73b3adbaaa7cd84c7993f86e8829a8fc100
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-09 18:36:20 +0100

    fix: env

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      APPWRITE_ENDPOINT: string;
+      APPWRITE_FUNCTION_API_ENDPOINT: string;
       APPWRITE_FUNCTION_PROJECT_ID: string;
       APPWRITE_FUNCTION_ID: string;
       DISCORD_PUBLIC_KEY: string;
--- functions/discord-bot/src/appwrite.js
+++ functions/discord-bot/src/appwrite.js
@@ -4,12 +4,12 @@ import { throwIfMissing } from './utils.js';
 export class Appwrite {
   constructor(apiKey) {
     throwIfMissing(process.env, [
-      'APPWRITE_ENDPOINT',
+      'APPWRITE_APPWRITE_FUNCTION_API_ENDPOINT',
       'APPWRITE_FUNCTION_PROJECT_ID',
     ]);
 
     this.client = new Client()
-      .setEndpoint(process.env.APPWRITE_ENDPOINT)
+      .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
       .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
       .setKey(apiKey);
   }
