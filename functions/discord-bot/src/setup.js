9490e91e542a76b1b7fa75e14f7e20c63da6030b
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-11 10:44:01 +0100

    debug

--- functions/discord-bot/src/setup.js
+++ functions/discord-bot/src/setup.js
@@ -2,11 +2,15 @@ import { commands } from './commands/index.js';
 import { Discord } from './services/discord.js';
 
 async function setup() {
+  console.log('Init Discord...');
   const discord = new Discord();
+  console.log('done');
 
   for (const command of commands) {
     try {
+      console.log(`Registering ${command.name} command...`);
       await discord.registerCommand(command.registration());
+      console.log(`done.`);
     } catch (error) {
       console.error(`Failed to register ${command.name} command:`, error);
     }
