be28be8c1e0f2d717fe2a8fc8f829ecdea3cd50f
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-11 14:03:30 +0100

    feat use geocodify

--- functions/discord-bot/src/services/geo.js
+++ functions/discord-bot/src/services/geo.js
@@ -1,42 +1,50 @@
-//import { fetchAdapter, OsmGeocoder } from '@spurreiter/geocoder';
 import { find } from 'geo-tz';
-//import countryEmoji from 'country-emoji';
+import countryEmoji from 'country-emoji';
 
 export class Geo {
-  async getCityFlag(cityName) {
-    return '🇺🇸';
-    /*
-    const adapter = fetchAdapter();
-    const geocoder = new OsmGeocoder(adapter, { language: 'en', limit: 5 });
-    try {
-      const res = await this.geocoder.forward(cityName);
-      if (res.length > 0) {
-        const countryCode = res[0].countryCode;
-        return countryEmoji.flag(countryCode) || '';
+  async forward(query) {
+    const params = new URLSearchParams({
+      api_key: process.env.GEOCODIFY_API_KEY,
+      q: query,
+    });
+
+    const response = await fetch(
+      `https://api.geocodify.com/v2/geocode?${params.toString()}`,
+      {
+        headers: {
+          Accept: 'application/json',
+        },
       }
-      return '';
-    } catch (error) {
-      console.error('Error:', error);
-      return '';
-    } */
+    );
+
+    return response.json();
+  }
+
+  async getCityFlag(cityName) {
+    const { response } = await this.forward(cityName);
+    const matches = response.features;
+
+    if (matches.length > 0) {
+      const countryCode = matches[0].properties.country_code;
+      return countryEmoji.flag(countryCode) || '';
+    }
+
+    return '';
   }
 
   async getCityTimezone(cityName) {
-    return find(40.7128, -74.006);
-    /*
-    try {
-      const res = await this.geocoder.forward(cityName);
-      if (res.length > 0) {
-        const { latitude, longitude } = res[0];
-        const timezone = find(latitude, longitude);
+    const { response } = await this.forward(cityName);
+    const matches = response.features;
 
-        if (timezone && timezone.length > 0) {
-          return timezone[0];
-        }
+    if (matches.length > 0) {
+      const latitude = matches[0].geometry.coordinates[1];
+      const longitude = matches[0].geometry.coordinates[0];
+      const timezone = find(latitude, longitude);
+
+      if (timezone && timezone.length > 0) {
+        return timezone[0];
       }
-      return false;
-    } catch (error) {
-      return false;
-    }*/
+    }
+    return false;
   }
 }
