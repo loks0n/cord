2394cfa04caee14e137c70dfad61e0b5e65e08be
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-11 13:23:25 +0100

    test

--- functions/discord-bot/src/services/geo.js
+++ functions/discord-bot/src/services/geo.js
@@ -1,14 +1,13 @@
-import { fetchAdapter, OsmGeocoder } from '@spurreiter/geocoder';
-//import { find } from 'geo-tz';
-import countryEmoji from 'country-emoji';
+//import { fetchAdapter, OsmGeocoder } from '@spurreiter/geocoder';
+import { find } from 'geo-tz';
+//import countryEmoji from 'country-emoji';
 
 export class Geo {
-  constructor() {
-    const adapter = fetchAdapter();
-    this.geocoder = new OsmGeocoder(adapter, { language: 'en', limit: 5 });
-  }
-
   async getCityFlag(cityName) {
+    return '🇺🇸';
+    /*
+    const adapter = fetchAdapter();
+    const geocoder = new OsmGeocoder(adapter, { language: 'en', limit: 5 });
     try {
       const res = await this.geocoder.forward(cityName);
       if (res.length > 0) {
@@ -19,27 +18,25 @@ export class Geo {
     } catch (error) {
       console.error('Error:', error);
       return '';
-    }
+    } */
   }
-
-  /* async getCityTimezone(cityName) {
-  try {
-    const res = await this.geocoder.forward(cityName);
-    if (res.length > 0) {
-      const { latitude, longitude } = res[0];
-      const timezone = find(latitude, longitude);
-
-      if (timezone && timezone.length > 0) {
-        return timezone[0];
-      }
-    }
-    return false;
-  } catch (error) {
-    return false;
-  }
-} */
 
   async getCityTimezone(cityName) {
-    return 'America/New_York';
-  }
+    return find(40.7128, -74.0060);
+    /*
+    try {
+      const res = await this.geocoder.forward(cityName);
+      if (res.length > 0) {
+        const { latitude, longitude } = res[0];
+        const timezone = find(latitude, longitude);
+
+        if (timezone && timezone.length > 0) {
+          return timezone[0];
+        }
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  } */
 }
